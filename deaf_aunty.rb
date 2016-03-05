d384a423355badb0548d13441946c7abcbb78072
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2016-03-05 17:25:27 +0800

    added deaf aunty code into repository
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

---
+++ deaf_aunty.rb
@@ -0,0 +1,14 @@
+# Save this file to your computer so you can run it
+# via the command line (Terminal) like so:
+#   $ ruby deaf_aunty.rb
+#
+# Your method should wait for user input, which corresponds
+# to you saying something to your Aunty.
+
+# You'll probably want to write other methods, but this
+# encapsulates the core Aunty logic
+def deaf_aunty
+end
+
+# Run our method
+deaf_aunty
