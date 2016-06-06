fc32b408c5652e79b409e86a050aa1747aff5823
ks2355244 <ks2355244@yahoo.com>
2016-06-06 16:10:29 +0800

    deaf aunty

--- deaf_aunty.rb
+++ deaf_aunty.rb
@@ -8,6 +8,14 @@
 # You'll probably want to write other methods, but this
 # encapsulates the core Aunty logic
 def deaf_aunty
+	puts "Do you want to shout at your Aunty? (Yes or No)"
+answer = gets.chomp
+
+puts "NO, WE CAN'T DO THAT" when answer == Yes
+puts "HUH?! SPEAK UP, SANDRA!" when answer == No
+
+end
+puts "I love ya, aunty, but I've got to go."
 end
 
 # Run our method
